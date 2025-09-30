08aca2b17ef6c7a3f0b0690b7e983cb60b043853
DreadDev1 <kameen.avery@gmail.com>
2025-09-30 14:39:07 -0400

    Section 2 Lecture 5 - Native Gameplay Tags

---
+++ Source/GASCrashCourse/Private/GameplayTags/GCC_Tags.cpp
@@ -0,0 +1,9 @@
+﻿#include "GameplayTags/GCC_Tags.h"
+
+namespace GCC_Tags
+{
+	namespace GCC_Abilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "GCC_Tags.GCC_Abilities.Primary", "Tag for the Primary Ability")
+	}
+}
---
+++ Source/GASCrashCourse/Public/GameplayTags/GCC_Tags.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+namespace GCC_Tags
+{
+	namespace GCC_Abilities
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Primary);
+	}
+}
