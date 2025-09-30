21dbdf9d87133838ec325deaac166cd2f2952ec4
DreadDev1 <kameen.avery@gmail.com>
2025-09-30 00:28:57 -0400

    Section 1 Lecture 6 Player Controller

---
+++ Source/GASCrashCourse/Public/Player/GCC_PlayerController.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GCC_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class GASCRASHCOURSE_API AGCC_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+#pragma region Input Actions Variables
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|MappingContext")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Input|Movement")
+	TObjectPtr<UInputAction> LookAction;
+#pragma endregion
+#pragma region Input Actions Functions
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+#pragma endregion
+};
