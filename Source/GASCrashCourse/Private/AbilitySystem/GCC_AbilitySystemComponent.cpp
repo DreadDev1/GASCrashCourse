fa2f090b2e53381c48be08a82f7898bb49e6a999
DreadDev1 <kameen.avery@gmail.com>
2025-10-04 02:50:55 -0400

    Section 3 Lecture - Custom Ability System Component

---
+++ Source/GASCrashCourse/Private/AbilitySystem/GCC_AbilitySystemComponent.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/GCC_AbilitySystemComponent.h"
+
+UGCC_AbilitySystemComponent::UGCC_AbilitySystemComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UGCC_AbilitySystemComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+
+// Called every frame
+void UGCC_AbilitySystemComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
