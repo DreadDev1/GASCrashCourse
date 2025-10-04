692b28ce28abb1a48edcb7989a606c87342b2f42
DreadDev1 <kameen.avery@gmail.com>
2025-10-04 02:43:32 -0400

    Section 3 Lecture 2 Enemy Class

---
+++ Source/GASCrashCourse/Public/Characters/GCC_EnemyCharacter.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GCC_BaseCharacter.h"
+#include "GCC_EnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class GASCRASHCOURSE_API AGCC_EnemyCharacter : public AGCC_BaseCharacter
+{
+	GENERATED_BODY()
+	
+public:
+	AGCC_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
