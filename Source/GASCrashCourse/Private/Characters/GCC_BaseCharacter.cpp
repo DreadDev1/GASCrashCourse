56bb29877681ac3eca5682e3f8c833830b4af241
DreadDev1 <kameen.avery@gmail.com>
2025-10-06 15:17:16 -0400

    Section 1 Lecture 3 Character Classes

---
+++ Source/GASCrashCourse/Private/Characters/GCC_BaseCharacter.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GASCrashCourse/Public/Characters/GCC_BaseCharacter.h"
+
+
+// Sets default values
+AGCC_BaseCharacter::AGCC_BaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+
+}
---
+++ Source/GASCrashCourse/Public/Characters/GCC_PlayerCharacter.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GCC_BaseCharacter.h"
+#include "GCC_PlayerCharacter.generated.h"
+
+UCLASS()
+class GASCRASHCOURSE_API AGCC_PlayerCharacter : public AGCC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	GACC_PlayerCharacter();
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	TObjectPtr<UCameraComponent> FollowCamera;
+};
