f7f8ba39272f614df7f495ddb60984833cfe7ff0
DreadDev1 <kameen.avery@gmail.com>
2025-10-06 19:25:21 -0400

    Section 3 Lecture 2 - Enemy Class

---
+++ Source/GASCrashCourse/Private/Characters/GCC_EnemyCharacter.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/GCC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+AGCC_EnemyCharacter::AGCC_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* AGCC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AGCC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!IsValid(GetAbilitySystemComponent())) return;
+
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+
+	if (!HasAuthority()) return;
+
+	GiveStartupAbilities();	
+}
