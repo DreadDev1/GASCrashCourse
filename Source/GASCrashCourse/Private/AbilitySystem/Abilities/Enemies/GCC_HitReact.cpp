b58ce3c9932a30fc8a28453eebe1c4f72f52c274
DreadDev1 <kameen.avery@gmail.com>
2025-10-06 23:26:44 -0400

    Section 3 Lecture 8 - Calculating Hit Direction

--- Source/GASCrashCourse/Private/AbilitySystem/Abilities/Enemies/GCC_HitReact.cpp
+++ Source/GASCrashCourse/Private/AbilitySystem/Abilities/Enemies/GCC_HitReact.cpp
@@ -2,3 +2,14 @@
 
 
 #include "AbilitySystem/Abilities/Enemies/GCC_HitReact.h"
+
+void UGCC_HitReact::CacheHitDirectionVectors(AActor* Instigator)
+{
+	AvatarForward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+
+	const FVector AvatarLocation = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const FVector InstigatorLocation = Instigator->GetActorLocation();
+
+	ToInstigator = InstigatorLocation - AvatarLocation;
+	ToInstigator.Normalize();
+}
--- Source/GASCrashCourse/Public/AbilitySystem/Abilities/Enemies/GCC_HitReact.h
+++ Source/GASCrashCourse/Public/AbilitySystem/Abilities/Enemies/GCC_HitReact.h
@@ -13,4 +13,14 @@ UCLASS()
 class GASCRASHCOURSE_API UGCC_HitReact : public UGCC_GamePlayAbility
 {
 	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Crash|Abilities")
+	void CacheHitDirectionVectors(AActor* Instigator);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Crash|Abilities")
+	FVector AvatarForward;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Crash|Abilities")
+	FVector ToInstigator;
 };
