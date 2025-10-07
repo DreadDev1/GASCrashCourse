676de37edbbc71e887cc05a9554771750ea9d448
DreadDev1 <kameen.avery@gmail.com>
2025-10-06 22:21:42 -0400

    Section 3 Lecture 6 - Overlap Test

---
+++ Source/GASCrashCourse/Public/AbilitySystem/Abilities/Player/GCC_Primary.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GCC_GamePlayAbility.h"
+#include "GCC_Primary.generated.h"
+
+
+
+
+UCLASS()
+class GASCRASHCOURSE_API UGCC_Primary : public UGCC_GamePlayAbility
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Crash|Abilities")
+	void HitBoxOverlapTest();
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxRadius = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxForwardOffset = 200.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxElevationOffset = 20.0f;
+};
